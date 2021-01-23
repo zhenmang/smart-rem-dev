ed942310ec63757a09af1dfb487ac81865f89e6a
luoyupu <815355586@qq.com>
2021-01-24 01:43:58 +0800

    feat: purify

--- public/index.js
+++ public/index.js
@@ -1,1 +1,65 @@
-"use strict";var _typeof=typeof Symbol==="function"&&typeof Symbol.iterator==="symbol"?function(e){return typeof e}:function(e){return e&&typeof Symbol==="function"&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e};(function(e,t){(typeof exports==="undefined"?"undefined":_typeof(exports))==="object"&&typeof module!=="undefined"?module.exports=t():typeof define==="function"&&define.amd?define(t):e=e||self})(undefined,function(){return function e(t){if(t===undefined){t=750}else{var n=!isNaN(t)&&typeof t==="number"&&t>0;if(!n){throw new Error("Argument of rem must be a number which greater than zero")}}var o=window;var i=o.document;var r=i.documentElement;var f=document.createElement("style");f.type="text/css";r.firstElementChild&&r.firstElementChild.appendChild(f);var u=void 0;var d=void 0;function m(){d=/Mobi/.test(navigator.userAgent)?r.getBoundingClientRect().width:r.clientWidth||i.body.clientWidth;var e=d/t*100;f.innerText="html{font-size:"+e+"px !important;}"}m();o.addEventListener?o.addEventListener("resize",function(){o.clearTimeout(u);u=o.setTimeout(m,288)},false):o.onresize=function(){o.clearTimeout(u);u=o.setTimeout(m,288)}}});
+/*!
+ * smart-rem.js
+ * (c) 2018 lyp
+ * Released under the MIT License.
+ */
+/* eslint-disable */
+module.exports = function smartRem (designWidth, designHeight) {
+
+  var len = arguments.length;
+  var tempArr = [];
+  if (len === 0) {
+    designWidth = 750;
+  }
+  var correctArg1 = tempArr.slice.call(arguments, 0).every(function(it) {
+    return typeof it === 'number' && !isNaN(it)
+  });
+  var correctArg2 = tempArr.slice.call(arguments, 0).every(function(it, index) {
+    var isStringNumber = typeof it === 'string' && !isNaN(+it.slice(0, -2));
+    if (isStringNumber) {
+      if (index === 0) {
+        designWidth = +(designWidth.includes('px') ? designWidth.slice(0, -2) : designWidth);
+      } else {
+        designHeight = +(designHeight.includes('px') ? designHeight.slice(0, -2) : designHeight);
+      }
+    }
+    return isStringNumber;
+  });
+  var isCorrect = correctArg1 || correctArg2;
+  if (!isCorrect) {
+    throw new Error('The correct usage of smart rem is smartRem(375, 667) or smartRem(375)');
+  }
+
+  var win = window;
+  var doc = win.document;
+  var docEl = doc.documentElement;
+  var remStyle = document.createElement('style');
+  var isMob = /Mobi/.test(navigator.userAgent);
+  var tid;
+  var width;
+  var rem;
+  docEl.firstElementChild && docEl.firstElementChild.appendChild(remStyle);
+
+  function refreshRem () {
+    var rect = docEl.getBoundingClientRect();
+    var body = doc.body;
+    width = isMob ? rect.width : (docEl.clientWidth || body.clientWidth);
+    height = isMob ? rect.height : (docEl.clientHeight || body.clientHeight);
+    if (designWidth && typeof designWidth === 'number') {
+      rem = (width / designWidth) * 100;
+    } else if (designHeight && typeof designHeight === 'number') {
+      rem = (height / designHeight) * 100;
+    } else {
+      throw new Error('The correct usage of smart rem is smartRem(375, 667) or smartRem(375)');
+    }
+    remStyle.innerText = 'html{font-size:' + rem + 'px !important;}';
+  }
+  refreshRem();
+  win.addEventListener ? (win.addEventListener('resize', function () {
+    win.clearTimeout(tid);
+    tid = win.setTimeout(refreshRem, 288);
+  }, false)) : win.onresize = function () {
+    win.clearTimeout(tid);
+    tid = win.setTimeout(refreshRem, 288);
+  }
+}
